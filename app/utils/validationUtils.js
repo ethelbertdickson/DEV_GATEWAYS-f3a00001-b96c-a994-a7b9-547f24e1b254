0186a42fb2aa9de2ab84cfd54829719f79960db7
ethelbertdickson <ethelbertdickson@gmail.com>
2023-07-17 05:19:27 +0100

    Joi validator added

--- app/utils/validationUtils.js
+++ app/utils/validationUtils.js
@@ -1,9 +1,15 @@
 // Validation utility functions
 
+const Joi = require('joi');
+
 const validateIpv4Address = (ipv4Address) => {
 	const ipv4Regex =
 		/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-	return ipv4Regex.test(ipv4Address);
+
+	const schema = Joi.string().pattern(ipv4Regex);
+
+	const { error } = schema.validate(ipv4Address);
+	return error === undefined;
 };
 
 module.exports = {
